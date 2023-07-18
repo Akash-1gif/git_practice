f9f675a7834f23f08b4c3732cc3aef632432a37a
Akash <akashadike15@gmail.com>
2023-07-18 18:33:08 +0530

    Fixed routing of all the web pages

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ x='homo sapiens'
 def entry_point1():
     return render_template('index.html',name_x=x)
 
-@app.route('/index.html')
+@app.route('/index')
 def entry_point_index():
     return render_template('index.html',name_x=x)
 
-@app.route('/about.html')
+@app.route('/about')
 def entry_point_about():
     return render_template('about.html')
 
-@app.route('/confess.html')
+@app.route('/confess')
 def entry_point_confess():
     return render_template('confess.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def entry_point_contact():
     return render_template('contact.html')
 
-@app.route('/posts.html')
+@app.route('/posts')
 def entry_point_posts():
     return render_template('posts.html')
 
-@app.route('/works.html')
+@app.route('/works')
 def entry_point_works():
     return render_template('works.html')
 
